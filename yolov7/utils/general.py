dc206c51d6929e100b818de70416a89290349477
hslee <lhhss0927@gmail.com>
2024-08-28 17:18:06 +0900

    cleaning

--- yolov7/utils/general.py
+++ yolov7/utils/general.py
@@ -612,6 +612,10 @@ def non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes=Non
     Returns:
          list of detections, on (n,6) tensor per image [xyxy, conf, cls]
     """
+    prediction = prediction[0][0]
+    # print(f"type(prediction): {type(prediction)}")
+    # print(f"prediction.shape: {prediction.shape}")
+        
 
     nc = prediction.shape[2] - 5  # number of classes
     xc = prediction[..., 4] > conf_thres  # candidates
